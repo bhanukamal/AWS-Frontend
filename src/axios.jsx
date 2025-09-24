aa422c82fe7d90af6fe7b8fdb78bac6468a4edae
bhanukamal <2300030287@kluniversity.in>
2025-09-24 14:31:24 +0530

    Update axios.jsx

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8081/api",
+  baseURL: "http://ec2-23-23-55-166.compute-1.amazonaws.com:8081/api",
 });
 delete API.defaults.headers.common["Authorization"];
 export default API;
